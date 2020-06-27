31e4fac54eab60f6da3fcb10968016d8ac811a74
goksanisil23 <goxan.isil@gmail.com>
2020-06-27 12:50:46 +0200

    adding simple odometry application with turtlebot

---
+++ Webots/SLAM_play_area/controllers/measure_model/build/release/measure_model.d
@@ -0,0 +1,8 @@
+build/release/measure_model.o: measure_model.c \
+ /home/gisil/webots/include/controller/c/webots/robot.h \
+ /home/gisil/webots/include/controller/c/webots/types.h \
+ /home/gisil/webots/include/controller/c/webots/nodes.h \
+ /home/gisil/webots/include/controller/c/webots/motor.h \
+ /home/gisil/webots/include/controller/c/webots/supervisor.h \
+ /home/gisil/webots/include/controller/c/webots/position_sensor.h \
+ /home/gisil/webots/include/controller/c/webots/inertial_unit.h
---
+++ analysis/plot_turtlebot_measure_model.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+measurement_file = open('/home/gisil/Work/SLAM/SLAM/analysis/measurement.txt', 'r') 
+ground_truth_file = open('/home/gisil/Work/SLAM/SLAM/analysis/ground_truth.txt', 'r') 
+
+x = []
+z = []
+yaw = []
+
+x_gt = []
+z_gt = []
+
+## parse the log file and populate variables
+while True: 
+	line_measurement = measurement_file.readline()
+	line_gt = ground_truth_file.readline()
+	
+	decoded_line_measurement = line_measurement.split()
+	decoded_line_gt = line_gt.split()
+
+	if(len(decoded_line_measurement) > 1):
+		x.append(float(decoded_line_measurement[0]))
+		z.append(float(decoded_line_measurement[1]))
+		yaw.append(float(decoded_line_measurement[2]))
+
+	if(len(decoded_line_gt) > 1):
+		x_gt.append(float(decoded_line_gt[0]))
+		z_gt.append(float(decoded_line_gt[1]))
+
+	if not line_measurement:
+		break
+
+####### plot the motion
+
+time = [t*0.1 for t in list(range(len(x)))]
+
+
+plt.plot(x, z, "b", label="trajectory")
+plt.plot(x_gt, z_gt, "r", label="GT")
+plt.legend()
+plt.xlabel("x")
+plt.ylabel("z")
+plt.grid(True)
+plt.show()
